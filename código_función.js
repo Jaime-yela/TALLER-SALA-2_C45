5dcb72d54384b70b4f2b7ea854ee85042cf3a94c
Matteo Uribe Pérez <matemine16@gmail.com>
2026-09-14 10:09:00 -0500

    función añadida en develop

---
+++ código_función.js
@@ -0,0 +1,4 @@
+// Abre una página en una ventana nueva con medidas específicas
+let miVentana = window.open("https://google.com", "nuevaVentana", "width=600,height=400");
+//código función en rama  
+//función añadida en repositorio  
