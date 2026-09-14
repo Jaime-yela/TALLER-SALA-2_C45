df14608270a902d70114e8a7f5dd87c0cd433aea
Matteo Uribe Pérez <matemine16@gmail.com>
2026-09-14 13:45:03 -0500

    se añadió función a la rama código_función.js

--- código_función.js
+++ código_función.js
@@ -1,2 +1,3 @@
 // Abre una página en una ventana nueva con medidas específicas
 let miVentana = window.open("https://google.com", "nuevaVentana", "width=600,height=400");
+//commit para función en jsq
